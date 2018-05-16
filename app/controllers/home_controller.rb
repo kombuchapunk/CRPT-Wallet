7ef519d96ed74c9df00ab68c69563a48496fb68c
Student <example@example.com>
2018-05-15 22:10:57 -0700

    add all necessary wallet info to home controller, calculate balance in USD

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,14 @@ class HomeController < ApplicationController
 before_action :authenticate_user!
 
   def index
-    response = BlockIo.get_address_by :label => current_user.wallet
-    @wallet = JSON.parse(response.to_json)
-    @network = @wallet["data"]["network"]
-    @address = @wallet["data"]["address"]
-    @balance = @wallet["data"]["available_balance"]
+    wallet_response = BlockIo.get_address_by :label => current_user.wallet
+    wallet = JSON.parse(wallet_response.to_json)
+    @network = wallet["data"]["network"]
+    @address = wallet["data"]["address"]
+    @balance = wallet["data"]["available_balance"].to_d
+    price_response = BlockIo.get_current_price :price_base => 'USD'
+    price = JSON.parse(price_response.to_json)
+    @price = price["data"]["prices"][0]["price"].to_d
   end
 
 end
